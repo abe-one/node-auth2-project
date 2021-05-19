f179c1227928dc1d8fe27ddc8603d164522c4967
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-18 20:54:45 -0500

    build out auth router

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,10 @@
+const { findBy } = require("../users/users-model");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+
+const nErr = (status, message) => {
+  return { [status]: status, [message]: message };
+}; //!experiment
 
 const restricted = (req, res, next) => {
   const token = req.headers.authorization;
@@ -13,45 +18,41 @@ const restricted = (req, res, next) => {
       }
     });
   } else {
-    next({ status: 401, message: "Token required" });
+    next(nErr(401, "Token required"));
   }
 };
 
-const only = (role_name) => (req, res, next) => {
+const only = (role_name) => (req, _res, next) => {
   req.decodedJwt.role_name === role_name
     ? next()
-    : next({ status: 403, message: "This is not for you" });
+    : next(nErr(403, "This is not for you"));
 };
 
-const checkUsernameExists = (req, res, next) => {
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"
-    }
-  */
+const checkUsernameExists = (req, _res, next) => {
+  const username = req.body.username;
+  findBy({ username })
+    .then(([user]) => {
+      if (user) {
+        req.foundUser = user;
+        next();
+      } else {
+        next(nErr(401, "Invalid credentials"));
+      }
+    })
+    .catch(next);
 };
 
-const validateRoleName = (req, res, next) => {
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
+const validateRoleName = (req, _res, next) => {
+  let role = req.body.role_name?.trim();
+  if (!role) {
+    role = "student";
+  } else if (role.length > 32) {
+    next(nErr(422, "Role name can not be longer than 32 chars"));
+  } else if (role === "admin") {
+    return next(nErr(422, "Role name can not be admin"));
+  }
+  req.body.role_name = role;
+  next();
 };
 
 module.exports = {
--- api/secrets/index.js
+++ api/secrets/index.js
@@ -1,3 +1,4 @@
 const JWT_SECRET = process.env.JWT_SECRET || "SET your .env variables";
+const BCRYPT_ROUNDS = process.env.BCRYPT_ROUNDS || 8;
 
-module.exports = { JWT_SECRET };
+module.exports = { JWT_SECRET, BCRYPT_ROUNDS };
