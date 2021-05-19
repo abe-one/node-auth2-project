6070dd9b1354700efd8d749799ca42903a62e253
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-18 19:25:57 -0500

    build only MW

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -18,16 +18,9 @@ const restricted = (req, res, next) => {
 };
 
 const only = (role_name) => (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
+  req.decodedJwt.role_name === role_name
+    ? next()
+    : next({ status: 403, message: "This is not for you" });
 };
 
 const checkUsernameExists = (req, res, next) => {
